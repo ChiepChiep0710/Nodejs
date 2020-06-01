cb0d5690c527a0be77e4627ed42324f4fe588a84
chiepchiep <hieunguyenmanh13.cdc@gmail.com>
2020-06-01 23:41:22 +0700

    update query parameters

--- query parameters/index.js
+++ query parameters/index.js
@@ -8,17 +8,24 @@ app.get('/',function(req,res){
         name: 'ChiepChiep'
     });
 });
+var  users=[
+    {id:1, name:'ChiepChiep'},
+    {id:2, name:'ChiepChiep1'}
+]
+
 app.get('/users', function(req,res){
     res.render('users/index',{
-        users:[
-            {id:1, name:'ChiepChiep'},
-            {id:2, name:'ChiepChiep1'}
-        ]
-
+       users: users
     });
 })
-app.get('/users/search'.function(req, res){
-    console.log(req.query)
+app.get('/users/search',function(req, res){
+    var q=req.query.q;
+    var matchedUsers= users.filter(function(user){
+        return user.name.indexOf(q) !== -1;
+    });
+    res.render('users/index',{
+        users: matchedUsers
+    })
 })
 app.listen(port,function(){
     console.log('sever listening on port '+port);
